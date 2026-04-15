c8289df36ff0aaa64a9cd211e81a92d54a754e70
Ssy123qwert <3184452577@qq.com>
2026-04-15 19:14:34 +0800

    Enable slider verification and modify wait logic

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ get_current_dayofweek = lambda action: (
 SLEEPTIME = 0.2  # 每次抢座的间隔
 ENDTIME = "20:01:00"  # 根据学校的预约座位时间+1min即可
 
-ENABLE_SLIDER = False  # 是否有滑块验证
+ENABLE_SLIDER = True  # 是否有滑块验证
 MAX_ATTEMPT = 5  # 最大尝试次数
 RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
 
@@ -81,14 +81,13 @@ def main(users, action=False):
         1 for d in users if current_dayofweek in d.get("daysofweek")
     )
 
-    # ===================== 【我加的：等待到 19:59:58 再开始抢，避免提前狂刷】 =====================
     target_hour = 19
     target_minute = 59
     target_second = 50
     target_wait=0
     logging.info(f"等待到 {target_hour:02d}:{target_minute:02d}:{target_second:02d} 再开始抢座...")
 
-    while  True:
+    while  False:
         now_ts = time.time() + (8 * 3600 if action else 0)
         now = time.localtime(now_ts)
         if (now.tm_hour == target_hour and
@@ -101,7 +100,6 @@ def main(users, action=False):
             logging.info("wait ")
 
     logging.info("时间到！开始抢座！")
-    # ==========================================================================================
 
     while current_time < ENDTIME:
         attempt_times += 1
